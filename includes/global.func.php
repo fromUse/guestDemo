baae72c7ed76ae3c53dfc3248901d5850816de54
formUse <838437017@qq.com>
2016-04-05 14:58:03 +0800

    此版本优化了验证码的字体样式,改用imagettftext()函数，优化字体在图片中坐标，调整图片大小为100 * 35

--- includes/global.func.php
+++ includes/global.func.php
@@ -21,67 +21,67 @@ function _runtime(){
 */
 function  _code( $width = 70,$height = 23,$size = 4 ){
 
-session_start();
-header('Content-type:image/png');
-//搜集验证码数据源
-$str = array(
- 3,4,5,6,7,8,
- a,b,c,d,e,f,h,i,j,k,m,n,o,p,r,s,t,u,v,w,x,y,
- A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
-);
-$code = '';
-//计算数组的长度
- $str_length = count($str);
-//设置字体
- $font = 6;
-//创建一个画布,画布默认颜色是黑色
- $_img = imagecreatetruecolor($width,$height);
- //文字的x,y坐标
- $x = 2;
- $y = 2;
-//创建一个指定颜色的画壁[背景]
- $back = imagecolorallocate( $_img,221,240,237);
- //把背景颜色填充到画布
- imagefill($_img,0,0,$back);
-  //随机生成字体颜色
+	session_start();
+	header('Content-type:image/png');
+	//搜集验证码数据源
+	$str = array(
+	 3,4,5,6,7,8,
+	 a,b,c,d,e,f,h,i,j,k,m,n,o,p,r,s,t,u,v,w,x,y,
+	 A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+	);
+	$code = '';
+	//计算数组的长度
+	 $str_length = count($str);
+	//设置字体
+	 $fontsize = 20;
+	//创建一个画布,画布默认颜色是黑色
+	 $_img = imagecreatetruecolor($width,$height);
+	 //文字的x,y坐标
+	 $x = $width/8;
+	 $y = $fontsize;
+	//创建一个指定颜色的画壁[背景]
+	 $back = imagecolorallocate( $_img,221,240,237);
+	 //把背景颜色填充到画布
+	 imagefill($_img,0,0,$back);
+		//随机生成字体颜色
 
-  for($i=0;$i<$size;$i++){
-    //随机生成y轴的偏移量
+		for($i=0;$i<$size;$i++){
+			//随机生成y轴的偏移量
 
-    $y += mt_rand(-($height/8),$height/8);
-    $fontcolor = imagecolorallocate($_img,mt_rand(0,200),mt_rand(0,200),mt_rand(0,200));
-    $index = mt_rand(0,$str_length);
-    $code_iten = $str[$index];
-    $code .= $code_iten;
-    imagestring($_img,$font,$x,$y,$code_iten,$fontcolor);
-      $x +=  $width/$size;
-  }
+			$y += mt_rand(-($height-$fontsize)/6,($height-$fontsize)/3);
+			$fontcolor = imagecolorallocate($_img,mt_rand(0,200),mt_rand(0,200),mt_rand(0,200));
+			$index = mt_rand(0,$str_length);
+			$code_iten = $str[$index];
+			$code .= $code_iten;
+			imagettftext($_img,$fontsize,0,$x,$y,$fontcolor,ROOT_PATH.'/ttf/Jackey_Egypt.ttf',$code_iten);
+			$x +=  $width/8+($width/16);
+		}
 
-  //把验证码保存到session中
-	//保存到session的全部小写
-    $_SESSION['code'] = strtolower($code);
-  //在图片上随机添加像素点
- for ($j=0; $j < 200; $j++)
- {
-   $pointcolor = imagecolorallocate($_img,mt_rand(0,200),mt_rand(0,200),mt_rand(0,200));
-   imagesetpixel($_img,mt_rand(0,69),mt_rand(0,69),$pointcolor);
- }
+		//把验证码保存到session中
+		//保存到session的全部小写
+			$_SESSION['code'] = strtolower($code);
+		//在图片上随机添加像素点
+	 for ($j=0; $j <40*$size; $j++)
+	 {
+		 $pointcolor = imagecolorallocate($_img,mt_rand(0,200),mt_rand(0,200),mt_rand(0,200));
+		 imagesetpixel($_img,mt_rand(0,$width),mt_rand(0,$height),$pointcolor);
+	 }
 
-  //给图片画出几条线
-  //新建一个线条用的画笔
-  $line_color = imagecolorallocate($_img,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
-  imageline($_img,mt_rand(0,10),mt_rand(0,10),mt_rand(0,68),mt_rand(0,24),$line_color);
+		//给图片画出几条线
+		//新建一个线条用的画笔
+		$line_color = imagecolorallocate($_img,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
+		imageline($_img,mt_rand(0,10),mt_rand(0,10),mt_rand(0,$width-10),mt_rand(0,$height/4),$line_color);
 
-  $line_color = imagecolorallocate($_img,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
-  imageline($_img,mt_rand(0,10),mt_rand(0,10),mt_rand(0,68),mt_rand(0,24),$line_color);
+		$line_color = imagecolorallocate($_img,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
+		imageline($_img,mt_rand(0,10),mt_rand(0,10),mt_rand(0,$width-10),mt_rand(0,$height/4),$line_color);
 
-  $line_color = imagecolorallocate($_img,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
-  imageline($_img,mt_rand(0,10),mt_rand(0,10),mt_rand(0,68),mt_rand(0,24),$line_color);
+		$line_color = imagecolorallocate($_img,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
+		imageline($_img,mt_rand(0,10),mt_rand(0,10),mt_rand(0,$width-10),mt_rand(0,$height/4),$line_color);
 
-//把图片输出到浏览器上
- imagepng($_img);
- //最后把资源句柄给释放掉
-imagedestroy($_img);
+	//把图片输出到浏览器上
+	 imagepng($_img);
+	 //最后把资源句柄给释放掉
+	imagedestroy($_img);
 }
 
 
@@ -102,7 +102,7 @@ function _callbackPage($msg){
 *	@param string user_name
 * @return string
 */
-function _checkUserName($user_name,$min=2,$max=20){
+function _checkUserName($user_name,$min=3,$max=20){
 		$user_name = trim($user_name);
 		$modle = "/[\<\>\ \^\@\!\%\#\*\(\)\-\=\+\《\》　]/";//特殊字符正则模式
 
@@ -182,7 +182,7 @@ function _checkPass_Tell($pass_tell){
 				_callbackPage('密码回答必须填');
 				exit();
 			}else if (mb_strlen($pass_tell,'utf-8') > 20) {
-				_callbackPage('密码回答在１～２0字内');
+				_callbackPage('密码回答在1～２0字内');
 				exit();
 			}
 			//给密码提示字符串进行转义
